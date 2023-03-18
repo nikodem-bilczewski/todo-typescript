76cc0e450734e229c8919923c9c6a20b09f9f9c4
nikodem bilczewski <bilczewski.nikodem@gmail.com>
2023-03-19 00:00:34 +0100

    initial commit

---
+++ dist/app.js
@@ -0,0 +1,5 @@
+// <li>
+//           <label for="task-1">Wyrzucić śmieci</label>
+//           <input type="checkbox" id="task-1" name="Wyrzucić śmieci" />
+//         </li>
+const tasks = ["Wyrzucić śmieci", "Pójść na trening", "Nakarmić psa"];
---
+++ src/app.ts
@@ -0,0 +1,6 @@
+// <li>
+//           <label for="task-1">Wyrzucić śmieci</label>
+//           <input type="checkbox" id="task-1" name="Wyrzucić śmieci" />
+//         </li>
+
+const tasks: string[] = ["Wyrzucić śmieci", "Pójść na trening", "Nakarmić psa"];
